2fb4a4234d429a96f30b5ffc79215be0457c0515
Julio César Fernández <jcfmunoz@icloud.com>
2021-03-28 20:14:38 +0200

    Initial Commit

---
+++ MVCBestPractices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCBestPractices
+//
+//  Created by Julio César Fernández Muñoz on 28/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
